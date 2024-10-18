8a828b9589ed6820824c55f8aef02a570882b7b6
Mohammad Arsalan Rather <arslanamin.org@gmail.com>
2024-10-18 11:09:14 +0530

    Commandline Args

--- basics/CommandLineArgs.java
+++ basics/CommandLineArgs.java
@@ -2,11 +2,11 @@ public class CommandLineArgs{
     public static void main(String args[]){
         if(args.length>0){
             for(int i=0;i<args.length;i++){
-                System.out.println
+                System.out.println("Assalamualikusm "+args[i]+", welcome to this Java Program!");
             }
         }
         else{
-            System.out.println("Please enter 1 or more names as command line args!")
+            System.out.println("Please enter 1 or more names as command line args!");
         }
     }
 }
