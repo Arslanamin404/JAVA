298aa6b0cad66a48d73f62dabe31290463c1d37d
Mohammad Arsalan Rather <arslanamin.org@gmail.com>
2024-11-05 16:08:02 +0530

    Least positive integers

---
+++ DescMathAssignment/LeastPositiveInteger.java
@@ -0,0 +1,53 @@
+/*
+ * We know that The smallest positive integer that can be expressed as 
+ todo: am+bn for all integers m and n is the GCD(a,b)
+*/
+
+public class LeastPositiveInteger {
+    public static void main(String[] args) {
+        System.err.print("-".repeat(50));
+
+        System.err.print("\nLeast positive integer of form 7m+6: ");
+        System.out.println(leastPositiveIntegerOfForm7nPlus6(7, 6));
+
+        System.err.print("\nLeast positive integer of form 7m+6n: ");
+        System.out.println(leastPositiveIntegerOfForm(7, 6));
+
+        System.err.print("\nLeast positive integer of form 65m+13n: ");
+        System.out.println(leastPositiveIntegerOfForm(65, 13));
+
+        System.err.print("\nLeast positive integer of form 18m-42n+96u: ");
+        System.out.println(leastPositiveIntegerOfForm(18, -42, 96));
+
+        System.err.print("-".repeat(50));
+    }
+
+    public static int leastPositiveIntegerOfForm7nPlus6(int multiplier, int constant) {
+        int n = 0;
+        int result = multiplier * n + constant;
+        return result;
+    }
+
+    public static int leastPositiveIntegerOfForm(int firstMultiplier, int secondMultiplier) {
+        return Math.abs(gcd(firstMultiplier, secondMultiplier));
+    }
+
+    public static int leastPositiveIntegerOfForm(int firstMultiplier, int secondMultiplier, int thirdMultiplier) {
+        return Math.abs(gcd(firstMultiplier, secondMultiplier, thirdMultiplier));
+    }
+
+    public static int gcd(int num1, int num2) {
+        while (num2 != 0) {
+            int remainder = num1 % num2;
+            num1 = num2;
+            num2 = remainder;
+        }
+        return Math.abs(num1);
+    }
+
+    public static int gcd(int num1, int num2, int num3) {
+        int gcdNum1Num2 = gcd(num1, num2);
+        return gcd(gcdNum1Num2, num3);
+    }
+
+}
