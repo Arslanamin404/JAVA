76ddb8920365e3270c51d4fb1ba9892adca92775
Arslanamin404 <arslanamin.org@gmail.com>
2024-11-21 20:01:47 +0530

    Decimal precession fixed to 2, using system.out.format()

--- Week_06/com/lab/main/MainApp.java
+++ Week_06/com/lab/main/MainApp.java
@@ -7,17 +7,25 @@ import com.lab.utils.ShapeUtils;
 public class MainApp {
     public static void main(String[] args) {
         Rectangle myRect = new Rectangle(50.0, 3.0);
+
         double rectangle_area = ShapeUtils.calculateArea(myRect);
         double rectangle_perimeter = ShapeUtils.calculatePerimeter(myRect);
-        System.err.println("Length: " + myRect.getLength());
+
+        System.out.println("Length: " + myRect.getLength());
         System.out.println("Breadth: " + myRect.getBreadth());
-        System.err.println("Area of Rectangle: " + rectangle_area);
-        System.err.println("Perimeter of Rectangle: " + rectangle_perimeter);
+
+        System.out.format("Area of Rectangle: %.2f%n", rectangle_area);
+        System.out.format("Perimeter of Rectangle: %.2f%n", rectangle_perimeter);
+
+        // ------------------------------------------------------------------------------------------------------------------------
 
         Circle circle = new Circle(15);
         double circle_area = ShapeUtils.calculateArea(circle);
         double circle_perimeter = ShapeUtils.calculatePerimeter(circle);
-        System.err.println("Area of Circle: " + circle_area);
-        System.err.println("Perimeter of Circle: " + circle_perimeter);
+
+        System.out.println("\n\nRadius: " + circle.getRadius());
+
+        System.out.format("Area of Circle: %.2f%n", circle_area);
+        System.out.format("Perimeter of Circle: %.2f%n", circle_perimeter);
     }
 }
